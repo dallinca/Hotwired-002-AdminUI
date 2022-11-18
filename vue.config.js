51837baa1e47fa60edc9ee5f1c5e45510f95038c
Dallin Andersen <andersen.dallin@gmail.com>
2022-11-18 07:32:45 -0700

    Set development mode port to 8081. Proxy API calls to API server in development mode.

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,12 @@ const path = require("path");
 const pathToRootPublic = "../Hotwired-003-AdminUICompiled/public/";
 
 module.exports = {
-    outputDir: pathToRootPublic
+    outputDir: pathToRootPublic,
+    devServer: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000'
+        }
+      }
+    }
 }
